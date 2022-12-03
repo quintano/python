a58aa2d04338f85b99612b7a24e2dac8b52a1675
John Pretto <john@pretto.com>
2022-12-03 09:36:41 -0800

    asdf
    asdf

---
+++ quiz-game.py
@@ -0,0 +1,15 @@
+print("Welcome")
+
+playing = input("Play? ")
+
+if playing != "yes":
+	quit()
+
+print("engage :")
+
+answer = input("What does CPU Standfor? ")
+if answer == "Central Processing Unit":
+	print('Correct')
+
+else: print("Incorrect")
+
